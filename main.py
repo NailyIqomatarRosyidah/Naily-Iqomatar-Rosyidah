7dc81678a9526ebf5ad4ab5a21b968f5157acb0f
Naily Iqomatar Rosyidah <80145738+NailyIqomatarRosyidah@users.noreply.github.com>
2023-03-21 08:09:56 +0000

    Update Biodata

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<p>Naily Iqomatar Rosyidah</p>"
+  return "<p>Biodata Diri</p><p>Nama : Naily Iqomatar Rosyidah </p><p>NIM : 22083000033</p><p>Prodi : Sistem Informasi</p><p>Tempat,tanggal lahir : Malang, 23 November 2003</p><p> Alamat : Dusun Ketapang, Kepanjen, Malang</p><p>Jenis Kelamin : Perempuan</p><p> kewarganegaraan : Indonesia</p><p>Agama : Islam</p><p>Instansi : Universitas Merdeka Malang</p>"
 
 
 if __name__ == "__main__":
